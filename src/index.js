27f23caf900e609a329fefc4e8cc4e3210ceae53
architg98 <archit.g1998@gmail.com>
2021-01-05 14:24:40 +0530

    Error handling done and conditions added on the basis of result

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null };
+        this.state = { lat: null, errMess: null };
 
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
+            (position) => {
+                this.setState({
+                    lat : position.coords.latitude
+                });
+            },
+            (err) => {
+                this.setState({
+                    errMess : err.message
+                });
+            }
         );
 
         // Checking push from VSCode
@@ -17,12 +25,27 @@ class App extends React.Component {
 
     render() {
         
-
-        return (
-            <div>
-                Latitude:
-            </div>
-        );
+            if(!this.state.lat && this.state.errMess) {
+                return (
+                    <div>
+                    Error: {this.state.errMess}
+                    </div>
+                );
+            }
+            if(this.state.lat && !this.state.errMess) {
+                return (
+                    <div>
+                    Latitude: {this.state.lat}
+                    </div>
+                );
+            }
+            else {
+                return (
+                    <div>
+                    Loading...
+                    </div>
+                );
+            }
     }
 }
 
